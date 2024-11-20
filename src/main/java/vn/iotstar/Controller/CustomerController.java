fd5f17720d3ae2dc2f843dbf9553c894730ee76e
hoanganh1401 <vohoanganh424@gmail.com>
2024-11-20 14:23:46 +0700

    h

---
+++ src/main/java/vn/iotstar/Controller/CustomerController.java
@@ -0,0 +1,41 @@
+package vn.iotstar.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.iotstar.model.Customer;
+
+@RestController
+@EnableMethodSecurity
+public class CustomerController {
+    final private List<Customer> customers = List.of(
+        Customer.builder().id("001").name("Võ Hoàng Anh").email("hoanganh424@gmail.com").build(),
+        Customer.builder().id("002").name("Hoàng Anh").email("tuonganh@gmail.com").build()
+    );
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello is Guest");
+    }
+
+    @GetMapping("/customer/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<List<Customer>> getCustomerList() {
+        List<Customer> list = this.customers;
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/customer/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<Customer> getCustomerList(@PathVariable("id") String id) {
+        List<Customer> customers = this.customers.stream().filter(customer ->
+            customer.getId().equals(id)).toList();
+        return ResponseEntity.ok(customers.get(0));
+    }
+}
